ca5f14ec9310ba39a489da2c1151cc325699fbf4
megganeturner <meggan.turner@hotmail.com>
2016-12-07 14:04:55 +1100

    home button added

--- js/two.js
+++ js/two.js
@@ -1,12 +1,18 @@
 var colors = ['#FF8268', '#41C0FF', '#3EFF84', '#FFD687', '#FF8B84', '#4886FF', '#AAE2FF', '#B4FFB4', '#FFE8BD', '#FFB4D3', '#C7BAFF', '#BCD7FF', '#84FF85', '#FFC880', '#FF7D6C', '#9100FF', '#fff'];
 
-
 $(document).ready(function(){
   var $body = $('body');
+  var $letters = $('.letters');
 
   $('.p').on('click', function(){
     var randy = colors[Math.floor(Math.random() * colors.length)];
     $('.letters').css('background-color', randy);
+  });
+
+  // NEEDS TO BE FIXED, ONLY FIRING ONCE
+  $letters.on('click', '.l', function(){
+    console.log('yay');
+    $letters.css('transform', 'rotate(180deg)');
   });
 
   $('.letters').mousemove(function(e){
